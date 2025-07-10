3f86a09c9da376ee6c2ce849d8328ec69e85f7f7
Foysal <ahmedmdfoysal123@gmail.com>
2025-07-10 11:19:22 +0600

    added redirect user to desire path after login functionality

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export const middleware = async (req) => {
+  const token = await getToken({ req });
+  const isTokenOk = Boolean(token);
+  const isUserAdmin = token?.role === "admin";
+  const isAdminSpecificRoute = req.nextUrl.pathname.includes("/dashboard");
+
+  if (isAdminSpecificRoute && !isUserAdmin) {
+    const callbackUrl = encodeURIComponent(req.nextUrl.pathname);
+    return NextResponse.redirect(
+      new URL(`/api/auth/signin?callbackUrl=${callbackUrl}`, req.url)
+    );
+  }
+
+  return NextResponse.next();
+};
